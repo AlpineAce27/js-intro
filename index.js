8ae7479b62e67cc61e422f4c63c189dc80ea05ba
AlpineAce27 <Kimballgoss@gmail.com>
2024-06-11 12:03:38 -0600

    added two files

---
+++ index.js
@@ -0,0 +1,7 @@
+//we talked about variables, types and conditions
+
+var thisVarbiable = 1 //this is outdated, never use var if you can help it
+let thisOtherVariable = 2 //this is the modern way to make a variable that will change. JavaScript automatically detects the type, in this case it is an integer
+const stringVariable = `string ${thisVarbiable} ${thisOtherVariable}` //strings 
+
+console.log(stringVariable)
